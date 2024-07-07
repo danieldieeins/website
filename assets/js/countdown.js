0e173cb1ee30a6ceecf35aad32b964ca7952ecfe
nerotvlive <daniel.niesmann@outlook.com>
2024-07-07 13:02:54 +0200

    Update countdown.js

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,28 +1,17 @@
 function updateCountdown(countdownGoal,id,endFunction) {
-    if(countdownGoal&&id&&endFunction) {
-        let countdownDate = new Date(countdownGoal);
-        const dateNow = new Date();
+    const distance = new Date(countdownGoal) - new Date();
 
-        const now = dateNow.getTime();
-        const distance = countdownDate.getTime() - now;
+    const days = Math.floor(distance / 86400000),
+        hours = Math.floor((distance % 86400000) / 3600000),
+        minutes = Math.floor((distance % 3600000) / 60000),
+        seconds = Math.floor((distance % 60000) / 1000);
 
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-            minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
-            seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById(id + "-days").innerText = (days > 0 ? days+" Tag" + (days !== 1 ? "en" : "") : "");
+    document.getElementById(id + "-hours").innerText = (hours > 0 ? hours+" Stunde" + (hours !== 1 ? "n" : "") : "");
+    document.getElementById(id + "-minutes").innerText = (minutes > 0 ? minutes+" Minute" + (minutes !== 1 ? "n" : "") : "");
+    document.getElementById(id + "-seconds").innerText = (seconds > 0 ? seconds+" Sekunde" + (seconds !== 1 ? "n" : "") : "");
 
-        const days_ = " Tag" + (days !== 1 ? "en" : ""),
-            hours_ = " Stunde" + (hours !== 1 ? "n" : ""),
-            minutes_ = " Minute" + (minutes !== 1 ? "n" : ""),
-            seconds_ = " Sekunde" + (seconds !== 1 ? "n" : "");
-
-        document.getElementById(id + "-days").innerText = days+days_;
-        document.getElementById(id + "-hours").innerText = hours+hours_;
-        document.getElementById(id + "-minutes").innerText = minutes+minutes_;
-        document.getElementById(id + "-seconds").innerText = seconds+seconds_;
-
-        if (distance <= 0 || String(days + " " + hours + " " + minutes + " " + seconds).includes("-") || String(days + " " + hours + " " + minutes + " " + seconds) === "0 0 0 0") {
-            endFunction();
-        }
+    if (distance <= 0) {
+        endFunction();
     }
 }
