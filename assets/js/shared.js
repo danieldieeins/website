89a9edbd7074968c6161e96e8b6b1fecf56ca698
danieldieeins <daniel.niesmann@outlook.com>
2025-01-24 22:26:06 +0100

    Update links

--- assets/js/shared.js
+++ assets/js/shared.js
@@ -1,6 +1,9 @@
 if(location.href.toLowerCase().includes("nerotvlive.nerofy.de")) {
     location.href = location.href.replaceAll("nerotvlive.nerofy.de","nerotvlive.nrfy.net");
 }
+if(location.href.toLowerCase().includes("nerotv.live")) {
+    location.href = location.href.replaceAll("nerotv.live","nerotvlive.nrfy.net");
+}
 if(location.href.toLowerCase().includes("nerotvlive.nrfy.net")&&location.href.toLowerCase().endsWith(".html")) {
     location.href = location.href.replaceAll(".html","");
 }
