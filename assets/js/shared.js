770d91af786a2dca3c6ebe24e70bf6fb0d8eaea4
nerotvlive <daniel.niesmann@outlook.com>
2025-06-25 19:00:04 +0200

    Hopefully redirect to new domain

--- assets/js/shared.js
+++ assets/js/shared.js
@@ -1,9 +1,9 @@
 if(location.href.toLowerCase().includes("nerotvlive.nerofy.de")) {
-    location.href = location.href.replaceAll("nerotvlive.nerofy.de","nerotvlive.nrfy.net");
+    location.href = location.href.replaceAll("nerotvlive.nerofy.de","nerotv.live");
 }
-if(location.href.toLowerCase().includes("nerotv.live")) {
-    location.href = location.href.replaceAll("nerotv.live","nerotvlive.nrfy.net");
+if(location.href.toLowerCase().includes("nerotvlive.nrfy.net")) {
+    location.href = location.href.replaceAll("nerotvlive.nrfy.net","nerotv.live");
 }
-if(location.href.toLowerCase().includes("nerotvlive.nrfy.net")&&location.href.toLowerCase().endsWith(".html")) {
+if(location.href.toLowerCase().includes("nerotv.live")&&location.href.toLowerCase().endsWith(".html")) {
     location.href = location.href.replaceAll(".html","");
 }
